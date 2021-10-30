0b6400547615c8d98d2d34210db25db6d55582ce
Victor Delgado <valexdelgado@gmail.com>
2021-10-30 11:59:57 -0700

    added mousefinder file, button click for 'activate cookies' button on the opening page

--- scratch.py
+++ scratch.py
@@ -8,9 +8,11 @@ jobLink = 'https://jobs.intel.com/page/show/search-results#t=Jobs&sort=relevancy
 browser = webdriver.Firefox(executable_path=r'C:\Users\valex\Downloads\geckodriver-v0.30.0-win64\geckodriver.exe')
 browser.get(jobLink)
 browser.maximize_window()
+sleep(2)
+#position of activate cookies button is x=1307 y=953
+pag.click(x=1307, y=953)
 
-
-sleep(10 )
+sleep(10)
 browser.quit()
 
 
