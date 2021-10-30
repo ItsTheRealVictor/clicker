dd9439f41dbdd2e021bf7851da59efa98ef62eca
Victor Delgado <valexdelgado@gmail.com>
2021-10-30 11:46:57 -0700

    added link to job search page

--- scratch.py
+++ scratch.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
 from time import sleep
+import pyautogui as pag
+
+
+jobLink = 'https://jobs.intel.com/page/show/search-results#t=Jobs&sort=relevancy&layout=table'
 
 browser = webdriver.Firefox(executable_path=r'C:\Users\valex\Downloads\geckodriver-v0.30.0-win64\geckodriver.exe')
-browser.get('https://github.com')
+browser.get(jobLink)
 browser.maximize_window()
+
+
 sleep(10 )
 browser.quit()
 
-# test test test test
+
